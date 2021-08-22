2f7f3fe25e433f400580b73c6ef7715a592d4b42
furkan <furkankeskinkilic97@gmail.com>
2021-08-22 19:59:16 +0300

    view partials part added

--- app.js
+++ app.js
@@ -10,18 +10,16 @@ app.use(express.static("public"));
 
 //Routes
 app.get('/', (req, res) => {
-  res.status(200).send('INDEX SAYFASI');
-  res.status(200).render('index', {
-      page_name: "index"
+  res.status(200).render('index',{
+  page_name : "index"
   });
 });
 
 app.get('/about', (req, res) => {
-    res.status(200).render('about', {
-        page_name: "about"
-    });
+  res.status(200).render('about',{
+    page_name: 'about'
   });
-
+});
 
 const port = 3000;
 app.listen(port,()=>{
