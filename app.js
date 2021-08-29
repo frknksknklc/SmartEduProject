567131935a57b11c289dd667615cbf1dd6ca6044
furkan <furkankeskinkilic97@gmail.com>
2021-08-29 19:16:27 +0300

    courses model and courses.ejs added

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
 const express = require('express');
-// express fonksiyonunu çalıştırıyoruz
-const app = express();
+const mongoose = require('mongoose')
 const pageRoute = require('./routes/pageRoute')
+const courseRoute = require('./routes/courseRoute')
+const app = express();
+//Connect DB
+mongoose.connect('mongodb://localhost:27017/smartedu-db', 
+    {useNewUrlParser: true,
+        
+    });
+
+// mongoose.connect('mongo://localhost:smartedu-db', {
+//     useNewUrlParser : true,
+//     useUnifiedTopology: true,
+//     useFindAndModify: true,
+//     useCreateIndex: true
+// }).then(()=>{
+//     console.log('DB connected succesfuly')
+// })
 
 //Template Engine
 app.set("view engine", "ejs");
 
 //Middlewares
 app.use(express.static("public"));
-
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 //Routes
 app.use('/', pageRoute);
+app.use('/courses', courseRoute);
 
 
 const port = 3000;
