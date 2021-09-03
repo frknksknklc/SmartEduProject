172c310ca3cd81a1e4095bbe98689afc1d659334
furkan <furkankeskinkilic97@gmail.com>
2021-09-03 12:34:41 +0300

    category created and listed

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose')
 const pageRoute = require('./routes/pageRoute')
 const courseRoute = require('./routes/courseRoute')
+const categoryRoute = require('./routes/categoryRoute')
+
 const app = express();
 //Connect DB
 mongoose.connect('mongodb://localhost:27017/smartedu-db', 
@@ -28,7 +30,7 @@ app.use(express.urlencoded({extended: true}))
 //Routes
 app.use('/', pageRoute);
 app.use('/courses', courseRoute);
-
+app.use('/categories', categoryRoute);
 
 const port = 3000;
 app.listen(port,()=>{
