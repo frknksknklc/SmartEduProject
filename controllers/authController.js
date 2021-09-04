2930b41fa2bc435c631e5b00f01ded8426ae9072
furkan <furkankeskinkilic97@gmail.com>
2021-09-04 23:36:29 +0300

    giris sayfası ve session eklendi

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,18 +1,72 @@
 const User = require('../models/User')
+const bcrypt = require('bcrypt')
+
 
 exports.createUser = async (req, res) => {
-    try{
+  try {
     const user = await User.create(req.body);
-    
-        res.status(201).json({
-            status : 'success',
-            user,
-        })
-    } catch(error) {
-        res.status(400).json({
-            status : 'fail',
-            error,
+
+    res.status(201).json({
+      status: 'success',
+      user,
     })
-    
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      error,
+    })
+
+  }
 }
-}
+
+// exports.loginUser = async (req, res) => {
+//   try {
+//     const {
+//       email,
+//       password
+//     } = req.body;
+
+//     await User.findOne({
+//       email
+//     }, (err, user) => {
+//       if (user) {
+//         bcrypt.compare(password, user.password, (err, same) => {
+//           if (same) {
+//             // USER SESSION
+//             req.session.userID = user._id;
+
+//             res.status(200).redirect('/');
+//           }
+//         });
+//       }
+//     });
+//   } catch (error) {
+//      res.status(400).json({
+//        status: 'fail',
+//        error,
+//      });
+//   }
+// }
+
+
+exports.loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user=  await User.findOne({ email });
+    if (user) {
+      bcrypt.compare(password, user.password, (err, same) => {
+        if (same) {
+          // USER SESSION
+          req.session.userID = user._id;
+          res.redirect('/');
+        }
+      });
+    }
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      error,
+    });
+  }
+};
