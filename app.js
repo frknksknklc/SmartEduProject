cbd2c95ba8b90fe6cae619c8e3872cd208754289
furkan <furkankeskinkilic97@gmail.com>
2021-09-04 23:52:56 +0300

    Logout eklendi

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose')
 const session = require('express-session')
+const MongoStore = require('connect-mongo');
 const pageRoute = require('./routes/pageRoute')
 const courseRoute = require('./routes/courseRoute')
 const categoryRoute = require('./routes/categoryRoute')
@@ -36,8 +37,10 @@ app.use(express.urlencoded({extended: true}))
 app.use(session({
     secret: 'my_keyboard_cat',
     resave: false,
-    saveUninitialized: true
+    saveUninitialized: true,
+    store: MongoStore.create({ mongoUrl: 'mongodb://localhost:27017/smartedu-db' })
   }))
+
 
 //Routes
 app.use('*',(req,res,next) =>{
